3a63e47a31b3290e74673bf822d9d18f2e21529a
Harim Zermeno <harimzermeno.z@gmail.com>
2021-12-15 14:58:40 -0800

    Added port variable

--- server.js
+++ server.js
@@ -30,4 +30,6 @@ app.use("/api/status", statusRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(process.env.PORT || 5000, () => console.log(`Server is running...`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
